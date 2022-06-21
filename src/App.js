9f941c79a19b6c2ceb8dbb3be696787545eb6b2e
Maleni A Hernandez <maleni.alexis@gmail.com>
2022-06-21 15:18:26 -0500

    cleaner code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from "react";
+import './index.css';
+import {useFormik} from "formik";
 // TODO: import useFormik from formik library
 
 function App() {
   // TODO: add a const called formik assigned to useFormik()
+    const formik = useFormik()
+    {
+        initialValues:
+        {
+            email: ''
+            password: ''
+        }
+        onSubmit: values => {console.log('form:', values);
+        }
+    }
 
   return (
-    <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
-    </div>
+      <div>
+          <form onSubmit={formik.handleSubmit}>
+              <div>Email</div>
+              {/*The email error message should be within a div element that has an id of emailError*/}
+              {/*name property & attribute with an onChange event*/}
+              <input id="emailField" name="email" type="email" onChange={formik.handleChange}
+                     value={formik.values.name}
+              />
+              {formik.errors.email ? <div>{formik.errors.email}</div> : null
+              }
+
+              <div>Password</div>
+              {/*The password error message should be within a div element that has an id of pswError*/}
+              <input id="pswField" name="password" type="password"/>
+              <button id="submitBtn" type="Submit">Submit</button>
+              {/*    The submit button should have an id of submitBtn*/}
+          </form>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
